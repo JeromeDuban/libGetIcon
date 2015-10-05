a2fdde81d5a08d817f28b083601f7cb4f20d9d61
jejefcgb <jduban@enseirb-matmeca.fr>
2015-10-05 21:11:40 +0200

    improved preview

--- app/src/main/java/fr/jeromeduban/libgeticon/MainActivity.java
+++ app/src/main/java/fr/jeromeduban/libgeticon/MainActivity.java
@@ -1,24 +1,27 @@
 package fr.jeromeduban.libgeticon;
 
-import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import fr.jeromeduban.getstoreicon.Manager;
 import fr.jeromeduban.getstoreicon.Parameter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 	
 	
 	public TextView txt;
-	public ImageView image1;
-	public ImageView image2;
-	public ImageView image3;
-	public ImageView image4;
-	public ImageView image5;
-	public ImageView image6;
+
+    private String[] packages = {"com.facebo  &=1ok.%20" ,"com.netmarble.mherosg","com.netmarble.mherosgb","com.facebook.katana","com.ustwo.monumentvalley","com.facebook.katana"};
+    private String[] result = {"Error", "Error", "Correct", "Correct", "Correct", "Correct"};
 
 	private Manager m;
 
@@ -27,15 +30,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		image1 = (ImageView) findViewById(R.id.image1);
-		image2 = (ImageView) findViewById(R.id.image2);
-		image3 = (ImageView) findViewById(R.id.image3);
-		image4 = (ImageView) findViewById(R.id.image4);
-		image5 = (ImageView) findViewById(R.id.image5);
-		image6 = (ImageView) findViewById(R.id.image6);
-
-		txt = (TextView)findViewById(R.id.tv);
-
 		Drawable defaultIcon = this.getResources().getDrawable(R.drawable.ic_launcher);
 		// OR
 		defaultIcon = this.getResources().getDrawable(R.mipmap.ic_launcher2);
@@ -43,17 +37,39 @@ public class MainActivity extends Activity {
 		Parameter param = new Parameter().setCache(true).setSize(100).setDefaultIcon(defaultIcon);
 
 		m = new Manager(this, param);
+        cleanCache(m);
 
-		m.deleteCache(this); //FIXME : to be removed
-//
-		m.download(image1, "com.facebo  &=1ok.%20");
-		m.download(image2,"com.netmarble.mherosg");
-		m.download(image3,"com.netmarble.mherosgb");
-		m.download(image4,"com.facebook.katana");
-		m.download(image5,"com.ustwo.monumentvalley");
-		m.download(image6,"com.facebook.katana");
+		LinearLayout container = (LinearLayout) findViewById(R.id.container);
+
+        for (int i = 0; i < packages.length; i++){
+
+            LayoutInflater in = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View card = in.inflate(R.layout.card, container, false);
+
+            TextView tv = (TextView) card.findViewById(R.id.packageName);
+            tv.setText(packages[i]);
+
+            TextView expectedResult = (TextView) card.findViewById(R.id.result);
+            expectedResult.setText(result[i]);
+            if (result[i].equals("error")) expectedResult.setTextColor(Color.RED);
+            else expectedResult.setTextColor(Color.GREEN);
+
+            ImageView iv = (ImageView) card.findViewById(R.id.icon);
+            
+            m.download(iv, packages[i]);
+
+            container.addView(card);
+        }
+
+
+
 
 	}
+
+    private void cleanCache(Manager m){
+        m.deleteCache(this);
+        Toast.makeText(MainActivity.this, "Cache cleaned", Toast.LENGTH_LONG).show();
+    }
 
 
 	@Override
